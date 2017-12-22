66e046f1bfc57245eaf415d9968f0b2567f251da
zhanglei53 <zhanglei53@jd.com>
2017-12-22 17:08:20 +0800

    页面

--- resources/900c3c5073a1d8cb4b9a3e0e072756c5.js
+++ resources/900c3c5073a1d8cb4b9a3e0e072756c5.js
@@ -1,54 +1,71 @@
 var json = {
-    "timeline": {
-        "headline": "The Main Timeline Headline Goes here",
-        "type": "default",
-        "text": "<p>Intro body text goes here, some HTML is ok</p>",
-        "asset": {
-            "credit": "Credit Name Goes Here",
-            "caption": "Caption text goes here"
+    "title": {
+        "media": {
+            "url": "//www.flickr.com/photos/tm_10001/2310475988/",
+            "caption": "Whitney Houston performing on her My Love is Your Love Tour in Hamburg.",
+            "credit": "flickr/<a href='http://www.flickr.com/photos/tm_10001/'>tm_10001</a>"
         },
-        "date": [
-            {
-                "startDate": "2011,12,10",
-                "endDate": "2011,12,10",
-                "headline": "Headline Goes Here",
-                "text": "<p>Body text goes here, some HTML is OK</p>",
-                "tag": "This is Optional"
+        "text": {
+            "headline": "Whitney Houston<br/> 1963 - 2012",
+            "text": "<p>Houston's voice caught the imagination of the world propelling her to superstardom at an early age becoming one of the most awarded performers of our time. This is a look into the amazing heights she achieved and her personal struggles with substance abuse and a tumultuous marriage.</p>"
+        }
+    },
+    "events": [
+        {
+            "media": {
+                "url": "{{ static_url }}/img/examples/houston/family.jpg",
+                "caption": "Houston's mother and Gospel singer, Cissy Houston (left) and cousin Dionne Warwick.",
+                "credit": "Cissy Houston photo:<a href='http://www.flickr.com/photos/11447043@N00/418180903/'>Tom Marcello</a><br/><a href='http://commons.wikimedia.org/wiki/File%3ADionne_Warwick_television_special_1969.JPG'>Dionne Warwick: CBS Television via Wikimedia Commons</a>"
             },
-            {
-                "startDate": "2011,12,11",
-                "endDate": "2011,12,11",
-                "headline": "Headline Goes Here",
-                "text": "<p>Body text goes here, some HTML is OK</p>",
-                "tag": "This is Optional"
+            "start_date": {
+                "month": "8",
+                "day": "9",
+                "year": "1963"
             },
-            {
-                "startDate": "2011,12,12",
-                "endDate": "2011,12,12",
-                "headline": "Headline Goes Here",
-                "text": "<p>Body text goes here, some HTML is OK</p>",
-                "tag": "This is Optional"
-            },
-            {
-                "startDate": "2011,12,13",
-                "endDate": "2011,12,13",
-                "headline": "Headline Goes Here",
-                "text": "<p>Body text goes here, some HTML is OK</p>",
-                "tag": "This is Optional"
+            "text": {
+                "headline": "A Musical Heritage",
+                "text": "<p>Born in New Jersey on August 9th, 1963, Houston grew up surrounded by the music business. Her mother is gospel singer Cissy Houston and her cousins are Dee Dee and Dionne Warwick.</p>"
             }
-        ]
-    }
+        },
+        {
+            "media": {
+                "url": "https://youtu.be/eH3giaIzONA",
+                "caption": "I Wanna Dance With Somebody",
+                "credit": "Arista Records Inc."
+            },
+            "start_date": {
+                "year": "1987"
+            },
+            "text": {
+                "headline": "'Whitney'",
+                "text": "Multiplatinum second album sells more than 20 million copies worldwide. With 'Whitney', Houston became the first female artist to produce four number 1 singles on one album including \"I Wanna Dance With Somebody,' 'Didn't We Almost Have It All,' 'So Emotional' and 'Where Do Broken Hearts Go.'"
+            }
+        },
+        {
+            "media": {
+                "url": "https://youtu.be/96aAx0kxVSA",
+                "caption": "\"One Moment In Time\" - Theme song to the 1988 Seoul Olympics",
+                "credit": "Arista Records Inc."
+            },
+            "start_date": {
+                "year": "1988"
+            },
+            "text": {
+                "headline": "\"One Moment In Time\"",
+                "text": "The artist's fame continues to skyrocket as she records the theme song for the Seoul Olympics, 'One Moment In Time.'"
+            }
+        }
+    ]
+};
+
+var options = {
+    start_at_end: true,
+    default_bg_color: {r: 0, g: 0, b: 0},
+    timenav_height: 250
 };
 
 $('#fullpage').hide();
 $('.right').hide();
 $('#timeline').show();
 
-createStoryJS({
-    type: 'timeline',
-    height: '600',
-    source: json,
-    embed_id: 'timeline',
-    lang: 'zh-cn',
-    font: 'BreeSerif-OpenSans'
-});
+window.timeline = new TL.Timeline('timeline', json, options);

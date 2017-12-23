2817c122a6d851107821fea80791289263c7974b
zhanglei53 <zhanglei53@jd.com>
2017-12-23 19:30:36 +0800

    页面

--- resources/900c3c5073a1d8cb4b9a3e0e072756c5.js
+++ resources/900c3c5073a1d8cb4b9a3e0e072756c5.js
@@ -6,8 +6,8 @@ var json = {
             "credit": "flickr/<a href='http://www.flickr.com/photos/tm_10001/'>tm_10001</a>"
         },
         "text": {
-            "headline": "Whitney Houston<br/> 1963 - 2012",
-            "text": "<p>Houston's voice caught the imagination of the world propelling her to superstardom at an early age becoming one of the most awarded performers of our time. This is a look into the amazing heights she achieved and her personal struggles with substance abuse and a tumultuous marriage.</p>"
+            "headline": "磊磊&静静 的爱情回忆录",
+            "text": "<p>遇到你是我的幸运，愿陪你走到天荒地老。 -- zl 2017-11-27</p>"
         }
     },
     "events": [
@@ -18,13 +18,13 @@ var json = {
                 "credit": "Cissy Houston photo:<a href='http://www.flickr.com/photos/11447043@N00/418180903/'>Tom Marcello</a><br/><a href='http://commons.wikimedia.org/wiki/File%3ADionne_Warwick_television_special_1969.JPG'>Dionne Warwick: CBS Television via Wikimedia Commons</a>"
             },
             "start_date": {
-                "month": "8",
-                "day": "9",
-                "year": "1963"
+                "month": "11",
+                "day": "27",
+                "year": "2017"
             },
             "text": {
-                "headline": "A Musical Heritage",
-                "text": "<p>Born in New Jersey on August 9th, 1963, Houston grew up surrounded by the music business. Her mother is gospel singer Cissy Houston and her cousins are Dee Dee and Dionne Warwick.</p>"
+                "headline": "我们的开始",
+                "text": "<p>11月27日中午，我通过同事认识了你，有句话叫“8杆子打不着边”，我3杆子就打着了。我们加了微信好友，下午我们侃侃而谈，期间还聊到了未来规划。。。那晚，我室友冒犯了你，你特别生气，在我的悉心安慰下，你终于平息了怒火，感觉有点hold不住你了。</p>"
             }
         },
         {
@@ -34,11 +34,13 @@ var json = {
                 "credit": "Arista Records Inc."
             },
             "start_date": {
-                "year": "1987"
+				"month": "12",
+				"day": "3",
+				"year": "2017"
             },
             "text": {
-                "headline": "'Whitney'",
-                "text": "Multiplatinum second album sells more than 20 million copies worldwide. With 'Whitney', Houston became the first female artist to produce four number 1 singles on one album including \"I Wanna Dance With Somebody,' 'Didn't We Almost Have It All,' 'So Emotional' and 'Where Do Broken Hearts Go.'"
+                "headline": "了解",
+                "text": "<p>之后的这一周多的时间，我们每天都聊天到很晚，互相说了很多话，有关于星座的、关于情感经历的、关于三观的、关于。。。，另外我们还深入探讨了天秤座-‘渣男星座’</p>"
             }
         },
         {
@@ -48,23 +50,119 @@ var json = {
                 "credit": "Arista Records Inc."
             },
             "start_date": {
-                "year": "1988"
+				"month": "12",
+				"day": "5",
+				"year": "2017"
             },
             "text": {
-                "headline": "\"One Moment In Time\"",
-                "text": "The artist's fame continues to skyrocket as she records the theme song for the Seoul Olympics, 'One Moment In Time.'"
+                "headline": "我的声音",
+                "text": "<p>你说你怂，我打电话给你，你说你很喜欢听我的声音，这是第一次有人告诉我喜欢听我的声音。</p>"
             }
-        }
+        },
+		{
+			"media": {
+				"url": "",
+				"caption": "\"One Moment In Time\" - Theme song to the 1988 Seoul Olympics",
+				"credit": "Arista Records Inc."
+			},
+			"start_date": {
+				"month": "12",
+				"day": "9",
+				"year": "2017"
+			},
+			"text": {
+				"headline": "第一次约会",
+				"text": "<p>第一次约会我们是在宋家庄站I口见面的，那天我们一起看了电影《寻梦环游记》，很好看你挑的，我们还一起吃了火锅。</p>"
+			}
+		},
+		{
+			"media": {
+				"url": "",
+				"caption": "\"One Moment In Time\" - Theme song to the 1988 Seoul Olympics",
+				"credit": "Arista Records Inc."
+			},
+			"start_date": {
+				"month": "12",
+				"day": "9",
+				"year": "2017"
+			},
+			"text": {
+				"headline": "第一次拒绝",
+				"text": "<p>这天晚上我说出了拒绝你的话，你很伤心，不再说话，那天晚上你还临时加了班。。。</p>"
+			}
+		},
+		{
+			"media": {
+				"url": "",
+				"caption": "\"One Moment In Time\" - Theme song to the 1988 Seoul Olympics",
+				"credit": "Arista Records Inc."
+			},
+			"start_date": {
+				"month": "12",
+				"day": "12",
+				"year": "2017"
+			},
+			"text": {
+				"headline": "第二次拒绝",
+				"text": "<p>这天晚上我第二次说出了拒绝你的话，你很伤心，我们决定做书友，并约好这周末一起到首图看书。</p>"
+			}
+		},
+		{
+			"media": {
+				"url": "",
+				"caption": "\"One Moment In Time\" - Theme song to the 1988 Seoul Olympics",
+				"credit": "Arista Records Inc."
+			},
+			"start_date": {
+				"month": "12",
+				"day": "15",
+				"year": "2017"
+			},
+			"text": {
+				"headline": "第三次拒绝",
+				"text": "<p>这天下午下班后，我在公司打台球，这是我第三次说出了拒绝你的话，你很伤心，我们决定做书友，并约好这周末一起到首图看书。</p>"
+			}
+		},
+		{
+			"media": {
+				"url": "",
+				"caption": "\"One Moment In Time\" - Theme song to the 1988 Seoul Olympics",
+				"credit": "Arista Records Inc."
+			},
+			"start_date": {
+				"month": "12",
+				"day": "17",
+				"year": "2017"
+			},
+			"text": {
+				"headline": "第二次约会",
+				"text": "<p>这是我们第二次约会，说好的看书，结果呢？两个人互相对眼看了一整天，晚上我还抱了你，你说你虚。。。</p>"
+			}
+		},
+		{
+			"media": {
+				"url": "",
+				"caption": "\"One Moment In Time\" - Theme song to the 1988 Seoul Olympics",
+				"credit": "Arista Records Inc."
+			},
+			"start_date": {
+				"month": "12",
+				"day": "19",
+				"year": "2017"
+			},
+			"text": {
+				"headline": "第三次约会",
+				"text": "<p>这是我们第三次约会，我一下班就去找你了，我们在蒲黄榆地铁站附近吃了饭，那天我手机莫名其妙出了问题，不能在美团下单了，还是你买的单呢。饭后本来只是散个步，无意到了一个公园，月黑风高，然后。。。</p>"
+			}
+		}
     ]
 };
 
 var options = {
-    start_at_end: true,
-    default_bg_color: {r: 0, g: 0, b: 0},
-    timenav_height: 250
+
 };
 
-$('#fullpage').hide();
+$('#fullpage').fadeOut(3000);
 $('.right').hide();
 $('#timeline').show();
 
